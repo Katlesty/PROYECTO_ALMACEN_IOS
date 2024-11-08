350b7fd44da431b84f5b3f345b221abc4461c674
Katlesty <yahaira.chavez@tecsup.edu.pe>
2024-11-08 17:41:37 -0500

    commit

---
+++ almacen_productos/EditarCategoriaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditarCategoriaViewController.swift
+//  almacen_productos
+//
+//  Created by Mac20 on 8/11/24.
+//
+
+import UIKit
+
+class EditarCategoriaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
